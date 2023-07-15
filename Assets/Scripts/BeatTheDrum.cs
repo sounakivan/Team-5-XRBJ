71a659cf836d17fced392cdaf974a1040478807a
Sounak Ghosh <58182994+sounakivan@users.noreply.github.com>
2023-07-15 15:58:02 -0500

    beat script edit

--- Assets/Scripts/BeatTheDrum.cs
+++ Assets/Scripts/BeatTheDrum.cs
@@ -2,24 +2,42 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class BeatTheDrum : MonoBehaviour
 {
     public GameObject beatPrefab;
     public Transform drum;
     private Vector3 drumPos;
+    private bool hasSpawnedBeat = false;
 
     public void Start()
     {
         drumPos = drum.position;
     }
-    private void OnCollisionEnter(Collision collision)
+
+    private void OnTriggerEnter(Collider other)
     {
         // Check if the collision is happening with the desired object
-        if (collision.gameObject.CompareTag("Guide"))
+        if (other.gameObject.CompareTag("Guide"))
         {
             // Instantiate the beatPrefab at the collision point
-            Instantiate(beatPrefab, drumPos, Quaternion.identity);
+            sendBeat();
+        }
+    }
+
+    private void sendBeat()
+    {
+        Instantiate(beatPrefab, drumPos, Quaternion.identity);
+        hasSpawnedBeat = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Reset the flag when exiting the collision
+        if (other.gameObject.CompareTag("Guide"))
+        {
+            hasSpawnedBeat = false;
         }
     }
 }
