68e4e5f9a1655ee35d8cb41f03041f0e272f394a
BAshinoff <58481618+BAshinoff@users.noreply.github.com>
2023-07-15 20:27:16 -0400

    Eye Blink Updates
    Updates to Eye Blink Code

---
+++ Assets/Scripts/CalculateActiveEyeBlinkRate.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CalculateActiveEyeBlinkRate : MonoBehaviour
+{
+    private List<int> LeftBlinksPerSecond = new List<int>();
+    private int totalLocalLeftBlinks = 0;
+    public int maxActiveListSize = 10; // Maximum size of the list
+
+    private float Active_LeftEyeBlink_Total;
+    private float Active_LeftEyeBlink_Rate;
+    private float Active_LeftEyeBlink_Ratio;
+    private float Active_LeftEyeBlink_PercentIncrease;
+    private float Baseline_LeftEyeBlink_Rate = CalculateBaselineEyeblinkRate.Baseline_LeftEyeBlink_Rate;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            totalLocalLeftBlinks++;
+        }
+
+        if (Time.frameCount % 60 == 0) // Calculate presses per second every 60 frames (1 second)
+        {
+            LeftBlinksPerSecond.Add(totalLocalLeftBlinks);
+            totalLocalLeftBlinks = 0;
+
+            // If the list size exceeds the maximum, remove the oldest value
+            if (LeftBlinksPerSecond.Count > maxActiveListSize)
+            {
+                LeftBlinksPerSecond.RemoveAt(0);
+            }
+
+            Debug.Log("Active Task, Local Left Blinks per Second: " + string.Join(", ", LeftBlinksPerSecond));
+        }
+
+        // Calculate Active, Local Blink Metrics
+
+        // Loop through the list and accumulate the sum
+        for (int i = 0; i < LeftBlinksPerSecond.Count; i++)
+        {
+            Active_LeftEyeBlink_Total += LeftBlinksPerSecond[i];
+        }
+
+        Active_LeftEyeBlink_Rate = Active_LeftEyeBlink_Total / LeftBlinksPerSecond.Count; // The tracking time needs to be in seconds, I am assuming this is the total time for tracking.
+        Active_LeftEyeBlink_Ratio = Active_LeftEyeBlink_Rate / Baseline_LeftEyeBlink_Rate;
+        Active_LeftEyeBlink_PercentIncrease = Active_LeftEyeBlink_Ratio * 100;
+
+        Debug.Log("Active Left Eye Blink Total: " + Active_LeftEyeBlink_Total);
+        Debug.Log("Active Left Eye Blink Rate: " + Active_LeftEyeBlink_Rate);
+        Debug.Log("Active Left Eye Blink Ratio: " + Active_LeftEyeBlink_Ratio);
+        Debug.Log("Active Left Eye Blink Percent Increase: " + Active_LeftEyeBlink_PercentIncrease);
+
+    }
+}
---
+++ Assets/Scripts/CalculateBaselineEyeblinkRate.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculateBaselineEyeblinkRate : MonoBehaviour
+{
+
+    private int totalBaselineLeftBlinks = 0;
+    internal static float Baseline_LeftEyeBlink_Rate;
+    private float StartTime;
+    private float ElapsedTime;
+    public float BaselineTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ElapsedTime = Time.time - StartTime; //I "Think" this is in seconds. Double check
+
+        if (ElapsedTime >= BaselineTime)
+        {
+            StopCount();
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                totalBaselineLeftBlinks++;
+                Debug.Log("Baseline Left Eye Blink Total: " + totalBaselineLeftBlinks);
+
+            }
+
+        }
+
+         
+    }
+    
+
+    private void StopCount()
+    {
+
+        Baseline_LeftEyeBlink_Rate = totalBaselineLeftBlinks / ElapsedTime; // The tracking time needs to be in seconds, I am assuming this is the total time for tracking.
+        
+        Debug.Log("Baseline Left Eye Blink Rate: " + Baseline_LeftEyeBlink_Rate);
+
+        enabled = false;
+
+    }
+}
